e69a01d101ed06881e3eeaf6daa2423ac8a7ecf0
Pål Saugstad <pal@saugstad.net>
2021-10-09 18:57:33 +0200

    Fix tests

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-rm facit
+echo $'\n'"These have a solution"$'\n'
 while read -r line ; do
-  python3 script.py $line >/tmp/res
+  python3 ../script.py $line >/tmp/res
   tail -2 /tmp/res | head -1
 done < test-file
+echo $'\n'"These haven't got a solution, but are 1 away"$'\n'
+while read -r line ; do
+  python3 ../script.py $line >/tmp/res
+  tail -2 /tmp/res | head -1
+done < test-one-away
