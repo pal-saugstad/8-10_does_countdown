69295bf73595b22f1febc588e5554bde03868a12
Pål Saugstad <pal@saugstad.net>
2021-10-10 11:56:04 +0200

    Test same test line again if test failed

--- tests/test.sh
+++ tests/test.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
 function do_test {
-  echo $'\n'$2$'\n'
+  echo $'\n'"$2 (result should be $3 away). Trying same input again if more that $3 away"$'\n'
   while read -r line ; do
-    python3 ../script.py $line >/tmp/res
-    printf "%-25s|" "$line"
-    tail -2 /tmp/res | head -1
+    while : ; do
+      failed=''
+      python3 ../script.py $line >/tmp/res
+      show=$(tail -2 /tmp/res | head -1)
+      if [ $3 -eq 0 ] ; then
+        [ "$(echo "$show" | grep 'away')" ] && failed=' FAILED '
+      else
+        want=$(echo $show | grep " $3 away")
+        [ "$want" ] || failed=' FAILED '
+      fi
+      printf "%-25s|%s%s\n" "$line" "$failed" "$show"
+      [ "$failed" ] || break
+    done
   done < $1
 }
 
-do_test test-file "These have a solution"
-do_test test-one-away "These haven't got a solution, but are 1 away"
+do_test test-file "These have a solution" 0
+do_test test-one-away "These haven't got a solution" 1
